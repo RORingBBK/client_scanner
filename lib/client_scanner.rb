60a09500b8ed66f61a0af3cb6c13f12d935fb98c
RORingBBK <bbk.programmerz@gmail.com>
2025-03-16 20:02:36 +1100

    Initial commit
    Run bundle gem client_scanner to setup new app.
    Rubocop, github and rspec chosen for coding convention, CI service and test framework.

---
+++ lib/client_scanner.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "client_scanner/version"
+
+module ClientScanner
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/client_scanner_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe ClientScanner do
+  it "has a version number" do
+    expect(ClientScanner::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
