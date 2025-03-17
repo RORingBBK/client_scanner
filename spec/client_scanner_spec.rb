2e5177e5ea2d230fd4e36ae539307a5ceacc05a0
RORingBBK <bbk.programmerz@gmail.com>
2025-03-17 21:18:43 +1100

    Remove test example where actual expectation is not provided

--- spec/client_scanner_spec.rb
+++ spec/client_scanner_spec.rb
@@ -4,8 +4,4 @@ RSpec.describe ClientScanner do
   it "has a version number" do
     expect(ClientScanner::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(false)
-  end
 end
