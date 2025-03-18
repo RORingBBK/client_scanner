cfe9bd3818142b19fe9c0c68fc9ea6077cb88d11
RORingBBK <bbk.programmerz@gmail.com>
2025-03-18 22:51:47 +1100

    Add spec for search_presenter

---
+++ spec/presenters/search_presenter.rb
@@ -0,0 +1,35 @@
+RSpec.describe Presenters::SearchPresenter, type: :presenter do
+  describe "#present" do
+    subject { described_class.new(results).present }
+
+    context "when results is nil" do
+      let(:results) { nil }
+
+      it { is_expected.to eq("No clients found") }
+    end
+
+    context "when results is empty" do
+      let(:results) { [] }
+
+      it { is_expected.to eq("No clients found") }
+    end
+
+    context "when results contains clients" do
+      let(:results) do
+        [
+          { name: "John Doe", email: "test@hola.com" },
+          { name: "Jane", email: "jane@hola.com" }
+        ]
+      end
+      let(:expected_output) do
+        <<~OUTPUT
+          Found 2 results:
+          - John Doe (test@hola.com)
+          - Jane (jane@hola.com)
+        OUTPUT
+      end
+
+      it { is_expected.to eq(expected_output) }
+    end
+  end
+end
