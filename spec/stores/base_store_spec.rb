32cc47f5eb9157e5b88933a11b483a8e6d9b29f3
RORingBBK <bbk.programmerz@gmail.com>
2025-03-18 23:52:33 +1100

    Define subject before let declarations

--- spec/stores/base_store_spec.rb
+++ spec/stores/base_store_spec.rb
@@ -1,7 +1,9 @@
-RSpec.describe Stores::BaseStore, type: :store do
-  let(:file_path) { "spec/fixtures/contacts.json" }
+# frozen_string_literal: true
 
+RSpec.describe Stores::BaseStore, type: :store do
   subject(:store) { described_class.new(file_path) }
+
+  let(:file_path) { "spec/fixtures/contacts.json" }
 
   describe "#load_data" do
     let(:error_message) { "Subclasses must implement the load_data method" }
