8d60edc8b14742ce5b3a552a21b062c69fe66367
RORingBBK <bbk.programmerz@gmail.com>
2025-03-18 22:04:40 +1100

    Use optparse to read and parse search and file arguments

--- lib/client_scanner.rb
+++ lib/client_scanner.rb
@@ -1,9 +1,34 @@
 # frozen_string_literal: true
 
-require_relative "client_scanner/version"
+require "optparse"
 require "debug"
+require_relative "client_scanner/version"
+require_relative "stores/client_store"
+require_relative "commands/search_command"
+require_relative "presenters/search_presenter"
 
 module ClientScanner
   class Error < StandardError; end
-  # Your code goes here...
+
+  class CLI
+    def initialize
+      @options = {}
+    end
+
+    def run(argv)
+      parser = OptionParser.new do |opts|
+        opts.banner = "Usage: bin/client_scanner [options]"
+
+        opts.on("-f FILE", "--file FILE", "Path to the file") do |file|
+          @options[:file] = file
+        end
+
+        opts.on("-s FIELD QUERY", "--search QUERY", "Search for a query") do |query|
+          @options[:search] = query
+        end
+      end
+
+      parser.parse!(argv)
+    end
+  end
 end
