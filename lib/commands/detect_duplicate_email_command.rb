90e2b9157aa0fcabf72fda21b898fca5cd0929fe
RORingBBK <bbk.programmerz@gmail.com>
2025-03-19 14:06:34 +1100

    Add command to detect duplicate email

---
+++ lib/commands/detect_duplicate_email_command.rb
@@ -0,0 +1,16 @@
+# frozed_string_literal: true
+
+module Commands
+  class DetectDuplicateEmailCommand < BaseCommand
+    def initialize(store, query)
+      super(store)
+
+      @store = store
+      @query = query
+    end
+
+    def execute
+      @store.detect_duplicate_email(@query)
+    end
+  end
+end
