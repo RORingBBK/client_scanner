b59a953dabcb1543f3c262ef4cfe7302746d52ff
RORingBBK <bbk.programmerz@gmail.com>
2025-03-19 14:12:58 +1100

    Refactor each parser options into separate method
    Avoids length methods for better readability

--- lib/client_scanner.rb
+++ lib/client_scanner.rb
@@ -40,14 +40,24 @@ module ClientScanner
     def setup_parser
       @parser.banner = "Usage: bin/client_scanner [options]"
 
+      add_file_option
+      add_search_option
+      add_email_option
+    end
+
+    def add_file_option
       @parser.on("-f FILE", "--file FILE", "Path to the file") do |file|
         @options[:file] = file
       end
+    end
 
+    def add_search_option
       @parser.on("-s FIELD QUERY", "--search QUERY", "Search for a query") do |query|
         @options[:search] = query
       end
+    end
 
+    def add_email_option
       @parser.on("-e EMAIL", "--email EMAIL", "Search for duplicates email") do |query|
         @options[:search] = query
         @search_command = Commands::DetectDuplicateEmailCommand
